c7c11d7f663537de5c2f1fbfe23607f84e484315
jackson916 <colingrey412@gmail.com>
2022-04-18 09:08:38 -0400

    created the event listners for the app

--- app/app.js
+++ app/app.js
@@ -1,9 +1,25 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
-
+const authEvents = require('./auth/events.js')
+const gamesEvents = require('./games/events.js')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  $('#sign-up-form').on('submit', authEvents.onSignUp)
+  $('#sign-in-form').on('submit', authEvents.onSignIn)
+  $('#sign-out-button').on('click', authEvents.onSignOut)
+  $('#change-password-form').on('submit', authEvents.onChangePassword)
+  $('#create-review').on('submit', gamesEvents.onCreateGames)
+  $('#update-review').on('submit', gamesEvents.onUpdateGames)
+  $('#delete-review').on('submit', gamesEvents.onDeleteGames)
+
+  $('#sign-out-button').hide()
+  $('#change-password-form').hide()
+  $('.or2').hide()
+  $('.cpheader').hide()
+  $('.upreview').hide()
+  $('#create-review').hide()
+  $('#update-review').hide()
+  $('#delete-review').hide()
 })
