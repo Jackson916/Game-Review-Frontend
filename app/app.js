4e4bff137f2e583eb304dbb0fc0fe7d09d8eb019
jackson916 <colingrey412@gmail.com>
2022-04-24 22:36:11 -0400

    update eventlistner

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 const authEvents = require('./auth/events.js')
-const gamesEvents = require('./games/events.js')
+const gameEvents = require('./game/events.js')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
@@ -10,9 +10,11 @@ $(() => {
   $('#sign-in-form').on('submit', authEvents.onSignIn)
   $('#sign-out-button').on('click', authEvents.onSignOut)
   $('#change-password-form').on('submit', authEvents.onChangePassword)
-  $('#create-review').on('submit', gamesEvents.onCreateGames)
-  $('#update-review').on('submit', gamesEvents.onUpdateGames)
-  $('#delete-review').on('submit', gamesEvents.onDeleteGames)
+
+  $('#create-review').on('submit', gameEvents.onCreateGame)
+  $('#update-review').on('submit', gameEvents.onUpdateGame)
+  $('#delete-review').on('submit', gameEvents.onDeleteGame)
+  $('#index-games').on('click', gameEvents.onIndexGame)
 
   $('#sign-out-button').hide()
   $('#change-password-form').hide()
@@ -22,4 +24,5 @@ $(() => {
   $('#create-review').hide()
   $('#update-review').hide()
   $('#delete-review').hide()
+  $('#index-games').hide()
 })
